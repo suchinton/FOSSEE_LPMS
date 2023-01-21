80b3d40765b41c5210cc269342c3d5203fd18314
suchinton <75079303+suchinton@users.noreply.github.com>
2023-01-21 16:24:29 +0530

    Resolving Dependencies

--- Ubunut_install_script.sh
+++ Ubunut_install_script.sh
@@ -4,5 +4,10 @@
 
 sudo apt install python3-pip
 sudo apt install verilator
+sudo apt-get install git perl python3 make autoconf g++ flex bison ccache
+sudo apt-get install libgoogle-perftools-dev numactl perl-doc
+sudo apt-get install libfl2  # Ubuntu only (ignore if gives error)
+sudo apt-get install libfl-dev  # Ubuntu only (ignore if gives error)
+sudo apt-get install zlibc zlib1g zlib1g-dev  # Ubuntu only (ignore if gives error)
 pip install pyinstaller
 pip install PyQt5
